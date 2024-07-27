29d61df5a5691d80da390acf9eef22acca19fd2b
Kate Yemialyanava <yemialyanavakate@gmail.com>
2024-07-27 15:11:57 +0300

    Add task

---
+++ src/leetCode/LC_206_ListNode.java
@@ -0,0 +1,9 @@
+package leetCode;
+
+public class LC_206_ListNode {
+    int val;
+     LC_206_ListNode next;
+      LC_206_ListNode() {}
+      LC_206_ListNode(int val) { this.val = val; }
+      LC_206_ListNode(int val, LC_206_ListNode next) { this.val = val; this.next = next; }
+}
